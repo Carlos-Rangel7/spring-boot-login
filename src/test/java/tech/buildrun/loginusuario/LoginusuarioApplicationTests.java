e3f845305ff0bb25a7ddcb97659f45b0506e3c40
Carlos Rangel <croi7726@gmail.com>
2024-12-07 17:05:33 -0300

    adicionando classe User com atributos r configurações JPA

---
+++ src/test/java/tech/buildrun/loginusuario/LoginusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.loginusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
